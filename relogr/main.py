a77631132b46ab9512c064b3156b6c84bbd1ba10
Rudraksh MK <rudraksh@paperkite.in>
2016-11-26 16:44:09 +0530

    Added comments.

--- relogr/main.py
+++ relogr/main.py
@@ -9,13 +9,17 @@ class RelogR(object):
     timestamp_format = None
     timezone = None
 
-    # Initialize the RelogR class with config info.
+
+    # Initialize the RelogR class with config info - a running RethinkDB instance's host & port, a format for storing timestamps
+    # and a timezone.
     def __init__(self, rethinkdb_host, rethinkdb_port, timestamp_format, timezone):
         self.rethinkdb_host = rethinkdb_host
         self.rethinkdb_port = rethinkdb_port
         self.timestamp_format = timestamp_format
         self.timezone = timezone
     
+
+    # Log an incoming message with the current timestamp in the relogr_logs table in a special RethinkDB database called relogr.
     def log(self, message):
         timestamp = a.now().to(self.timezone).format(self.timestamp_format)
         r.connect(rethinkdb_host, rethinkdb_port).repl()
