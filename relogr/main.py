7f13bdd337ea13b3d52dea686b05ceb29ce0ff58
Rudraksh MK <rudraksh@paperkite.in>
2016-11-26 16:40:33 +0530

    Added a timezone attribute to the RelogR class. Added a log method that takes the current timestamp & message, converts it to the timezone and format specified earlier, and pushes it to RethinkDB.

--- relogr/main.py
+++ relogr/main.py
@@ -1,17 +1,28 @@
 import rethinkdb as r
 import arrow as a
+import collections
 
 class RelogR(object):
     # Store config info as class attributes.
     rethinkdb_host = None
     rethinkdb_port = None
     timestamp_format = None
+    timezone = None
 
     # Initialize the RelogR class with config info.
-    def __init__(self, rethinkdb_host, rethinkdb_port, timestamp_format):
+    def __init__(self, rethinkdb_host, rethinkdb_port, timestamp_format, timezone):
         self.rethinkdb_host = rethinkdb_host
         self.rethinkdb_port = rethinkdb_port
         self.timestamp_format = timestamp_format
+        self.timezone = timezone
     
-    
+    def log(self, message):
+        timestamp = a.now().to(self.timezone).format(self.timestamp_format)
+        r.connect(rethinkdb_host, rethinkdb_port).repl()
+        payload = collections.OrderedDict()
+        payload['timestamp'] = timestamp
+        payload['message'] = message
+        r.db('relogr').table('relogr_logs').insert(payload).run()
+        return True
+
     
