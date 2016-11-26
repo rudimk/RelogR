dfbe0c207e210117aa16a74d943369b17ed70218
Rudraksh MK <rudraksh@paperkite.in>
2016-11-26 16:54:23 +0530

    Added a setup package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='relogr',    
+    version='0.1',
+    description='A generic logger with RethinkDB as the primary logstore.',
+    url='https://github.com/rudimk/relogr',
+    author='Rudraksh MK',
+    author_email='rudraksh.mk@gmail.com',
+    license='MIT',
+    packages=['relogr'],
+    zip_safe=False)
